8d12b9f4ec5ca8bba2ab0a4d20e4f74b40cd4e03
dstoutin97 <64677344+DrewS97@users.noreply.github.com>
2021-08-27 19:45:42 +0000

    Initial commit

--- main.cs
+++ main.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace Practice2 {
   class MainClass {
     public static void Main (string[] args) {
+      //Counts the charcters in a predefined string
       Alice.ListChars();
     }
   }
